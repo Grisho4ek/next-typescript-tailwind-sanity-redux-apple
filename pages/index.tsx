b1055c614d6737e11a03431eed86aa059ffef874
Grygorii <shevchenko.grygorii@gmail.com>
2023-03-15 00:12:56 +0100

    render categories tabs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import { Tab } from '@headlessui/react';
-import type { GetServerSideProps, NextPage } from 'next';
+import type { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Header from '../components/Header';
 import Landing from '../components/Landing';
+import { fetchCategories } from '../utils/fetchCategories';
 
-const Home: NextPage = () => {
+interface Props {
+  categories: Category[];
+}
+
+const Home = ({ categories }: Props) => {
   return (
     <div>
       <Head>
@@ -22,21 +27,21 @@ const Home: NextPage = () => {
           </h1>
           <Tab.Group>
             <Tab.List className='flex justify-center'>
-              {/* {categories.map((category) => (
+              {categories.map((category) => (
                 <Tab
                   key={category._id}
                   id={category._id}
                   className={({ selected }) =>
                     `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
                       selected
-                        ? 'borderGradient bg-[#35383C] text-white'
+                        ? 'borderGradient bg-[#35383C] pb-[5px] text-center text-white'
                         : 'border-b-2 border-[#35383C] text-[#747474]'
                     }`
                   }
                 >
-                  iPhone
+                  {category.title}
                 </Tab>
-              ))} */}
+              ))}
             </Tab.List>
             <Tab.Panels className='mx-auto max-w-fit pt-10 pb-24 sm:px-4'>
               {/* <Tab.Panel className='tabPanel'>{showProducts(0)}</Tab.Panel>
@@ -53,9 +58,11 @@ const Home: NextPage = () => {
 
 export default Home;
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  // const categories = await fetchCategories()
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const categories = await fetchCategories();
   return {
-    props: {},
+    props: {
+      categories,
+    },
   };
 };
